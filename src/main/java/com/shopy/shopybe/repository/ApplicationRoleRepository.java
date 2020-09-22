e96d64239ae2a704cbc592d15911cd81ebaf6230
ciordan <Catalin.Iordan@endava.com>
2020-09-22 22:03:46 +0300

    #33: Create roles and permissions. Implement a repository interface and a  service class to access roles.

--- src/main/java/com/shopy/shopybe/repository/ApplicationRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopy.shopybe.repository;
-
-import com.shopy.shopybe.entity.ApplicationRole;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicationRoleRepository extends CrudRepository<ApplicationRole, Integer> {
-}
